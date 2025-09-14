2cd8b2325f5af50fa8ae9af7ec81846ef41ea216
dieutrinh28 <trinhdo.work@gmail.com>
2025-09-14 17:57:57 +0700

    add log error

--- src/main/java/com/das/skillmatrix/exception/GlobalExceptionHandler.java
+++ src/main/java/com/das/skillmatrix/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.das.skillmatrix.exception;
 
+import org.slf4j.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,9 +13,11 @@ import jakarta.security.auth.message.AuthException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(AuthException.class)
     public ResponseEntity<ApiResponse<Object>> handleAuthException(AuthException e) {
+        log.error("Authentication failed - Error: {}", e.getMessage());
 
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), 401);
         ApiResponse<Object> response = new ApiResponse<>(null, false, errorResponse);
@@ -24,6 +27,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Object>> handleGenericException(Exception e) {
+        log.error("Unexpected error - Error: {}", e.getMessage());
 
         ErrorResponse errorResponse = new ErrorResponse("An unexpected error occurred: " + e.getMessage(), 500);
         ApiResponse<Object> response = new ApiResponse<>(null, false, errorResponse);
