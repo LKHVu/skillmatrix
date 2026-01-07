f0d8f42e0d98edfacbc0efe67a9d64b7075ca3d6
PhanLaThanhPhong <phanlathanhphong19@gmail.com>
2026-01-07 10:44:37 +0700

    feature(team): add team management

--- src/main/java/com/das/skillmatrix/repository/TeamRepository.java
+++ src/main/java/com/das/skillmatrix/repository/TeamRepository.java
@@ -1,5 +1,8 @@
 package com.das.skillmatrix.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,8 @@ import com.das.skillmatrix.entity.Team;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    boolean existsByName(String name);
     
+    @EntityGraph(attributePaths = "manager")
+    List<Team> findAll();
 }
