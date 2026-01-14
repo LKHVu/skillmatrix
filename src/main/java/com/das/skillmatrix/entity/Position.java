3516bf846df86468213b66933809f7fa4cdf717e
PhanLaThanhPhong <phanlathanhphong19@gmail.com>
2026-01-14 15:59:15 +0700

    feature(skill): skill management

--- src/main/java/com/das/skillmatrix/entity/Position.java
+++ src/main/java/com/das/skillmatrix/entity/Position.java
@@ -4,12 +4,17 @@
  */
 package com.das.skillmatrix.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -35,4 +40,8 @@ public class Position {
 
     private String name;
     private String description;
+
+    @OneToMany(mappedBy = "position", fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST,
+            CascadeType.MERGE }, orphanRemoval = true)
+    private List<PositionSkill> positionSkills;
 }
